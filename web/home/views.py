545ad69c3711870465139423f7d0de220940e7df
alicool91 <68010649+alicool91@users.noreply.github.com>
2020-07-08 19:30:12 +0530

    Update views.py

--- web/home/views.py
+++ web/home/views.py
@@ -1,15 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-	print(request.user)
-	return render(request, "home.html", {})
+	context ={}
+	return render(request, "home.html", context)
 
 def team_view(request, *args, **kwargs):
-	print(request.user)
-	return render(request, "team.html", {})
+	context = {}
+	return render(request, "team.html", context)
 
 def about_view(request, *args, **kwargs):
-	print(request.user)
-	return render(request, "about.html", {})
+	context = {}
+	return render(request, "about.html", context)
