b9aa593300ccbc8a9b6778ce2bac088680871d19
alicool91 <68010649+alicool91@users.noreply.github.com>
2020-07-08 17:39:57 +0530

    Add files via upload

---
+++ web/result/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def result_view(request, *args, **kwargs):
+	context = {
+		'a':'Heloo'
+	}
+	return render(request, "result.html", context)
+
