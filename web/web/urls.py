b1ed7d662a5f39ad40998c3cc257c37954e0d36e
alicool91 <68010649+alicool91@users.noreply.github.com>
2020-07-08 17:33:46 +0530

    Add files via upload

---
+++ web/web/urls.py
@@ -0,0 +1,27 @@
+"""portfunio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from home.views import home_view, team_view, about_view
+from result.views import result_view
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('result/', result_view, name='result'),
+    path('team/', team_view, name='team'),
+    path('about/', about_view, name='about'),
+    path('admin/', admin.site.urls),
+]
