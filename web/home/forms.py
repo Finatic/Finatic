87d8575f6baa1f231f26f89b42b04f5f9343dd80
Ali Nalawala <beatstar360@github.com>
2020-07-17 02:15:49 +0530

    m

--- web/home/forms.py
+++ web/home/forms.py
@@ -5,3 +5,6 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
+
+class port_opti(forms.Form):
+    pass
