934939dd9d4ec17f2f51f9ee9e134fc21d63abf7
Ali Nalawala <beatstar360@github.com>
2020-07-17 00:19:15 +0530

    form

--- web/home/urls.py
+++ web/home/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('about', views.about, name="about"),
     path('', views.home, name="home"),
-    path('result', views.result, name="result"),
+    path('try', views.result, name="result"),
     path('contact', views.contact_us, name="contact"),
     path('Portfolio_Optimiser/Portfolio_Optimization.html', views.Portfolio_Optimization,
          name="Portfolio_Optimization"),
